9f03a75ae3676bf11110a1b54619b356f4fa15d0
COSMOS <50519740+AmakiSora@users.noreply.github.com>
2022-01-31 23:55:38 +0800

    RapidDev 1.1.3 增加上传聊天文件接口

--- quotations/Quotations.py
+++ quotations/Quotations.py
@@ -1,7 +1,8 @@
+import re
 from datetime import datetime
 from typing import Optional
 
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, File
 from pydantic import BaseModel
 
 """
@@ -14,6 +15,16 @@ app_quotation = APIRouter()
 @app_quotation.get('/{name}')
 def get_quotation(name):
     return name
+
+
+# 上传语录
+@app_quotation.post('/upload')
+async def upload_quotation(file:UploadFile = File(...)):
+    content = await file.read()
+    one = re.finditer(r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} [^\n]*", content.decode('utf-8'))
+    for i in one:
+        print(i.group())
+    return ''
 
 
 # 语录类
