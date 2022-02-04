91a936e68e55cbd7d5a6cf96ab188af30a867191
NOCOSMOS <50519740+NOCOSMOS@users.noreply.github.com>
2022-02-05 03:39:52 +0800

    RapidDev 1.1.10 返回版本信息接口

--- RapidRun.py
+++ RapidRun.py
@@ -13,5 +13,12 @@ app = FastAPI()
 
 app.include_router(app_quotation, prefix='/quotation', tags=['语录集'])
 
+
+# HelloWorld
+@app.get('/')
+def RapidDev():
+    return 'This is RapidDev V1.1.10'
+
+
 if __name__ == '__main__':
     uvicorn.run(app='RapidRun:app', host='127.0.0.1', port=2233, reload=True, debug=True, workers=1)
